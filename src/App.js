3d43bd5d2254f7d9de7fd2836eed91f634ad36fa
njj <njj@laufire.in>
2022-07-27 19:13:39 +0530

    Introduced a router.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import { React } from 'react';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import './App.scss';
+import About from './components/about';
+import Home from './components/home';
 
+// eslint-disable-next-line max-lines-per-function
 const App = () =>
-	<div className="App" role="App">
-		Ready to start.
-	</div>;
+	<BrowserRouter>
+		<ul>
+			<li>
+				<Link to="/">Home</Link>
+			</li>
+			<li>
+				<Link to="/about">About</Link>
+			</li>
+		</ul>
+		<main>
+			<Switch>
+				<Route exact={ true } path="/">
+					<Home/>
+				</Route>
+				<Route path="/about">
+					<About/>
+				</Route>
+			</Switch>
+		</main>
+	</BrowserRouter>;
 
 export default App;
